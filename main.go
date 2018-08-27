16b9406ff6ded204aad1896f3c2c20c9291125ab
Shalauddin Ahamad Shuza <shuza.sa@gmail.com>
2018-08-28 01:01:27 +0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+/**
+ * :=  created by:  Shuza
+ * :=  create date:  28-Aug-2018
+ * :=  (C) CopyRight Shuza
+ * :=  www.shuza.ninja
+ * :=  shuza.sa@gmail.com
+ * :=  Fun  :  Coffee  :  Code
+ **/
+
+func main()  {
+	fmt.Println("Hi Learn GoLang")
+}
